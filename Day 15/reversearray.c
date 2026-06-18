c5e3d93cbe6c3daa5d4b5d5daf56daf7f84d4caa
priyagupta0708 <priyagupta5966@gmail.com>
2026-06-18 19:38:43 +0530

    update

---
+++ Day 15/reversearray.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5], i;
+
+    printf("Enter 5 elements:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array in reverse order:\n");
+
+    for(i = 4; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
