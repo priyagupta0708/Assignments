3784a41360f709edd451581278f1d8a1297ef9c9
priyagupta0708 <priyagupta5966@gmail.com>
2026-06-18 19:41:11 +0530

    update

---
+++ Day 19/transposematrices.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[2][2], i, j;
+
+    printf("Enter elements of matrix:\n");
+
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Transpose Matrix:\n");
+
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            printf("%d ", a[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
